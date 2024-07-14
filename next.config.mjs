78cbacbdc8cc08f1955397feb2b970dad8c6407c
Al Siddeg Omer <alsiddeg.omer19990@gmail.com>
2024-07-14 11:52:55 +0300

    edits

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-// next.config.mjs
-
-/** @type {import('next').NextConfig} */
 const config = {
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
+    distDir: '.next',
   };
   
   export default config;
