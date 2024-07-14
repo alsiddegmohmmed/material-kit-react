6f113bf2b3cc7d1ad0cfe712763505d7cfe624e2
Al Siddeg Omer <alsiddeg.omer19990@gmail.com>
2024-07-14 12:18:00 +0300

    another update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
+/** @type {import('next').NextConfig} */
 const config = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    distDir: '.next',
-  };
-  
-  export default config;
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: '.next',
+};
+
+module.exports = config;
